7413510a646833c1b7cb9caaf2d018044cd41815
Henry Moule <hen10@hotmail.co.uk>
2022-02-20 22:45:12 +0000

    added more assets

---
+++ Assets/Collision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hello world");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hello world trigger");
+    }
+}
